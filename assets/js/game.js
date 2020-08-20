22cd3d99ceba9be3e7b93bd963726aae0940c655
aznjp <jpark103193@gmail.com>
2020-08-20 12:12:18 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,6 +27,12 @@ for (var i = 0; i < enemyNames.length; i++) {
     console.log(enemyNames[i] + " is at " + i + " index");
 }
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
+};
+
 
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
@@ -49,7 +55,9 @@ var fight = function(enemyName) {
         }
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
+
         console.log(
             playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
@@ -59,7 +67,7 @@ var fight = function(enemyName) {
             window.alert(enemyName + ' has died!');
 
             // award player money for winning
-            playerMoney = playerMoney + 20;
+            playerMoney = Math.max(0, playerMoney - 10);
 
             // leave while() loop since enemy is dead
             break;
@@ -68,7 +76,9 @@ var fight = function(enemyName) {
         }
 
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
+
         console.log(
             enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
@@ -101,7 +111,7 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
 
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
 
             // use debugger to pause script from running and check what's going on at that moment in the code
             // debugger;
@@ -150,6 +160,7 @@ var endGame = function() {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
+
 
 var shop = function() {
     // ask player what they'd like to do
